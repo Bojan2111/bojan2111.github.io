2da6ae13e5828257f98cff5b106f6829c74a17e5
Bojan2111 <bojan.adzic@gmail.com>
2022-05-25 22:04:16 +0200

    Update and add files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ export default function Footer() {
   return (
     <footer>
       <div>This is footer section</div>
+      <div>This is another footer section</div>
+      <div>This is yet another footer section</div>
     </footer>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import logo from '../logo.png';
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <img src={logo} alt="Letters A B M and T designed in rounded squares surrounded by big letter C"/>
-        ABMT Code
+    <header id='header'>
+      <div className='header--title'>
+        <img className='title-logo' src={logo} alt="Letters A B M and T designed in rounded squares surrounded by big letter C"/>
+        <span className='title'>ABMT Code</span>
       </div>
       <Navbar />
     </header>
